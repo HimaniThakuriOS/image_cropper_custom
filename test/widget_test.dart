d25b56398a1a98e157d1c6fce2d589a9122bf36f
Himani Thakur <himani.thakur@kellton.com>
2024-07-31 16:55:56 +0530

    modified code.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:image_cropper_custom_code/main.dart';
+import 'package:image_cropper_custom/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
